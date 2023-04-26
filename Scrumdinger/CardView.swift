9ccb3a8e662faad3747fa0a93eaac135bd371acc
ykhade <ykhade@nevada.unr.edu>
2023-04-26 07:18:21 -0700

    Adding card  view

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -10,16 +10,23 @@ import SwiftUI
 struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
-        VStack(alignment: .leading){
+        VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) meeting minutes")
+                    .labelStyle(.trailingIcon)
             }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 

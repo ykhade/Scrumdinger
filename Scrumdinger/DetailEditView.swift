da3548bc28827d903fee30c41efd5e9d82025681
ykhade <ykhade@nevada.unr.edu>
2023-05-01 15:47:57 -0700

    Adding edit view

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,33 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Yash Khade on 5/1/23.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")){
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
